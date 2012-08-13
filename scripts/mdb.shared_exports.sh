c84ce17822a4a92b26c27b574d040ee4e934e231
Matt Bridges <mbridges.91@gmail.com>
2012-08-12 20:13:07 -0500

    Updating shared exports to only export certain things when using OSX

--- scripts/mdb.shared_exports.sh
+++ scripts/mdb.shared_exports.sh
@@ -3,17 +3,17 @@ export BUNDLER_EDITOR='vi'
 # Customize to your needs...
 export PATH=/usr/local/bin:$PATH
 
-export PATH=$HOME/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
+export PATH=$HOME/bin:$PATH
 export PATH=$HOME/local/bin:$PATH
 export PATH=$HOME/.rbenv/bin:$PATH
 
-export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
-export PATH=/Applications/MAMP/Library/bin:$PATH
-export PATH=/Applications/MAMP/bin/php/php5.2.17/bin:$PATH
-export PATH=/Applications/MAMP/bin/php/php5.3.6/bin:$PATH
-
-# Postgres.app
 if [[ `uname` == 'Darwin' ]]; then
+  export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
+  export PATH=/Applications/MAMP/Library/bin:$PATH
+  export PATH=/Applications/MAMP/bin/php/php5.2.17/bin:$PATH
+  export PATH=/Applications/MAMP/bin/php/php5.3.6/bin:$PATH
+
+  # Postgres.app
   export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
 fi
