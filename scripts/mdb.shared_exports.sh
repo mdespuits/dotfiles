6df271c7591cb87da95f0c8801b388628e8e62da
Matt Bridges <mbridges.91@gmail.com>
2012-11-12 11:25:03 -0600

    fixing whitespace in shared exports

--- scripts/mdb.shared_exports.sh
+++ scripts/mdb.shared_exports.sh
@@ -8,12 +8,12 @@ export PATH=$HOME/bin:$PATH
 export PATH=$HOME/local/bin:$PATH
 export PATH=$HOME/.rbenv/bin:$PATH
 
-if [[ `uname` == 'Darwin' ]]; then
-  export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
-  export PATH=/Applications/MAMP/Library/bin:$PATH
-  export PATH=/Applications/MAMP/bin/php/php5.2.17/bin:$PATH
-  export PATH=/Applications/MAMP/bin/php/php5.3.6/bin:$PATH
-
-  # Postgres.app
-  export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
-fi
+# if [[ `uname` == 'Darwin' ]]; then
+#   export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
+#   export PATH=/Applications/MAMP/Library/bin:$PATH
+#   export PATH=/Applications/MAMP/bin/php/php5.2.17/bin:$PATH
+#   export PATH=/Applications/MAMP/bin/php/php5.3.6/bin:$PATH
+#
+#   # Postgres.app
+#   export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
+# fi
