5fbaa074e57cb3086817cbe3e6f34aaae5fb15d7
Matt Bridges <mbridges.91@gmail.com>
2013-03-03 16:32:24 -0600

    use hub for github only if hub is installed

---
+++ scripts/hub.bash
@@ -0,0 +1,2 @@
+# Hub for github
+# if which > /dev/null; then; eval "$(hub alias -s)"; fi
--- scripts/use-rbenv.bash
+++ scripts/use-rbenv.bash
@@ -1,6 +1,3 @@
 # Rbenv with Homebrew
 eval "$(rbenv init -)"
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-# Hub for github
-eval "$(hub alias -s)"
