524e99eb5e1c861b8fbcc019af3ef0f8e3e7cb82
Matt Bridges <mbridges.91@gmail.com>
2013-03-03 08:25:29 -0600

    moving linux specifics into scripts/linux.bash

--- scripts/mac.bash
+++ scripts/mac.bash
@@ -6,4 +6,5 @@ if [[ $platform == 'mac' ]]; then
   # Adding /url/local/bin for Homebrew
   export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
   export PATH="/usr/local/bin:$PATH"
+  export CC='gcc-4.2'
 fi
--- scripts/platform.bash
+++ scripts/platform.bash
@@ -8,9 +8,3 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
   platform='mac'
 fi
 
-if [[ $platform == 'linux' ]]; then
-  export CC='/usr/bin/gcc'
-  export PATH=$PATH:$HOME/bin:/sbin
-else
-  export CC='gcc-4.2'
-fi
