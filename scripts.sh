8cc44f88dc6047c4c70df3067fd23706c9104242
Matt Bridges <mbridges.91@gmail.com>
2013-03-06 15:07:54 -0600

    adding bin directory and simple file download command

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,17 @@
+export DOTIFY_BIN=$DOTIFY_HOME/bin
 export DOTIFY_SCRIPTS=$DOTIFY_HOME/scripts
 
 source "$DOTIFY_SCRIPTS/platform.bash"
+
+chmod 500 $DOTIFY_BIN;
+
+for file in $DOTIFY_BIN/*; do
+  if [ -f $file ] ; then
+    chmod 500 $file
+  fi
+done
+
+export PATH="$DOTIFY_BIN:$PATH"
 
 for file in $DOTIFY_SCRIPTS/*; do
   if [ -f "$file" ] ; then
