55ab55a016ec15c3340ab02c905e23a9fa002e4d
Matthew Wells <wells.md@pm.me>
2021-07-16 10:00:43 -0500

    zsh: Still want a good default fzf command

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -80,8 +80,7 @@ function tmlocal() {
 
 export FZF_TMUX_HEIGHT="40%"
 export FZF_DEFAULT_OPTS="--height 30% --layout=reverse"
-
-# export FZF_DEFAULT_COMMAND='ag -U --hidden -g "" --ignore .git --ignore tmp --ignore "*.png" --ignore "*.jpg" --ignore "node_modules" --ignore .bin --ignore .DS_Store --ignore "*.gif"'
+export FZF_DEFAULT_COMMAND='ag -U --hidden -g "" --ignore .git --ignore tmp --ignore "*.png" --ignore "*.jpg" --ignore "node_modules" --ignore .bin --ignore .DS_Store --ignore "*.gif"'
 
 # =========================
 # Autojumping with z
